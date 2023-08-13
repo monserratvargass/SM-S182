884374ea501d28ea67745fb9b0a467dc9bb8eb71
monserratvargass <guvamonse@gmail.com>
2023-08-13 16:03:28 -0600

    Rutas añadidas y logica para hacer registro

--- Examen 3erPacial/app.py
+++ Examen 3erPacial/app.py
@@ -92,8 +92,44 @@ def inicio():
 
 @app.route('/guardarPaciente',methods=['GET','POST'])
 def guardarPaciente():
+    if request.method=='POST': #Peticiones del usuario a traves del metodo POST
+        _medico=request.form['medico']
+        _paciente=request.form['paciente']
+        _ap=request.form['AP']
+        _am=request.form['AM']
+        _fn=request.form['FN']
+        _ec=request.form['EC']
+        _al=request.form['AL']
+        _af=request.form['AF']
+        #print(titulo,artista,anio)
+        CS=mysql.connection.cursor()
+        CS.execute('insert into expediente_paciente(medico,paciente,ap,am,fn,ec,al,af) values(%s,%s,%s,%s,%s,%s,%s,%s)',
+                   (_medico,_paciente,_ap,_am,_fn,_ec,_al,_af)) #Para ejecutar codigo sql, y pasamos parametros
+        mysql.connection.commit()
 
+    flash('Paciente registrado exitosamente') #este mnsj se imprime en el CRUD de albums
+    #return redirect(url_for('index')) #Reedireccionamiento a la vista index
     return render_template('expediente_paciente.html')
+
+@app.route('/guardarMedico',methods=['GET','POST'])
+def guardarMedico():
+    if request.method=='POST': #Peticiones del usuario a traves del metodo POST
+        _nombre=request.form['nombre']
+        _ap=request.form['AP']
+        _am=request.form['AM']
+        _cp=request.form['CP']
+        _pass=request.form['pass']
+        _ce=request.form['CE']
+        _rol=request.form['rol']
+        #print(titulo,artista,anio)
+        CS=mysql.connection.cursor()
+        CS.execute('insert into registrar_usuario(nombre,ap,am,cedulap,contrasena,correo,rol) values(%s,%s,%s,%s,%s,%s,%s)',
+                   (_nombre,_ap,_am,_cp,_pass,_ce,_rol)) #Para ejecutar codigo sql, y pasamos parametros
+        mysql.connection.commit()
+
+    flash('Médico registrado exitosamente')
+    #return redirect(url_for('index')) #Reedireccionamiento a la vista index
+    return render_template('registrar_medico.html')
 
 @app.route('/exploracionPaciente',methods=['GET','POST'])
 def exploracionPaciente():
@@ -110,6 +146,26 @@ def actualizarPaciente():
 
     return render_template('actualizar_paciente.html')
 
+@app.route('/buscarMedico',methods=['GET','POST'])
+def buscarMedico():
+    Bnom=request.form.get('user_RFC')
+    curBusq=mysql.connection.cursor()
+    curBusq.execute('select * from medico where rfc LIKE %s', (f'%{Bnom}%',))
+    busqueda=curBusq.fetchall()
+    return render_template('buscar_medico.html', busqmed=busqueda)
+
+#Consultar todas las citas que ha tenido el paciente, filtrada por el nombre del paciente y/o fecha de la cita
+@app.route('/consultarCita',methods=['GET','POST'])
+def consultarCita():
+    Cnom=request.form.get('user_nombre')
+    curSelect=mysql.connection.cursor()
+    curSelect.execute('select id from persona where nombre=? ',(Cnom,))
+    consulta=curSelect.fetchone()
+    if consulta:
+        curSelect.execute('select * from expediente_paciente where id_persona=?',(consulta[0],))
+        Ccitas=curSelect.fetchall()
+    return render_template('consultar_citas.html',tbcita=Ccitas)
+
 @app.route('/consultarCita',methods=['GET','POST'])
 def consultarCita():
 
@@ -124,6 +180,11 @@ def consultarPaciente():
 def descargarReceta():
 
     return render_template('descargar_receta.html')
+
+@app.route('/eliminarMedico',methods=['GET','POST'])
+def eliminarMedico():
+
+    return render_template('eliminar_medico.html')
 
 # ERROR HTML PARA SOLICITUDES MAL FORMADAS (ERROR 400)
 @app.errorhandler(400)
